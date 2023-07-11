f6b0dc2efd4e0647c263628dcf3a9231624d49f9
Derek <derekdanielson84@gmail.com>
2023-07-11 10:23:22 -0600

    Added event listener for button

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+
+function generateJoke(){
+    console.log(1123);
+}
+
+jokeBtn.addEventListener('click', generateJoke);
+
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.chucknorris.io/');
+xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
 
+
+xhr.onreadystatechange = function (){
+    if (this.readyState === 4 && this.status === 200){
+
+        const data = JSON.parse(this.responseText);
+
+        data.forEach((joke) => {
+            const li = document.createElement('li');
+            li.innerHTML = `<strong>${value}</strong>`;
+            document.querySelector('#results').appendChild(li);
+        });
+    }
+}
+
+xhr.send();
