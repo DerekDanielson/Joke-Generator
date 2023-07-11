1a77221d18dae0f80627db3c8a522e9299458e47
Derek <derekdanielson84@gmail.com>
2023-07-11 10:32:01 -0600

    JS complete

--- app.js
+++ app.js
@@ -3,27 +3,22 @@ const jokeBtn = document.getElementById('joke-btn');
 
 
 function generateJoke(){
-    console.log(1123);
-}
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');    
+
+    xhr.onreadystatechange = function (){
+        if (this.readyState === 4 && this.status === 200){
+            jokeEl.innerHTML = JSON.parse(this.responseText).value;
+            
+        }
+    };
+    xhr.send();
+};
 
 jokeBtn.addEventListener('click', generateJoke);
 
-const xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+document.addEventListener('DOMContentLoaded', generateJoke);
 
 
-xhr.onreadystatechange = function (){
-    if (this.readyState === 4 && this.status === 200){
 
-        const data = JSON.parse(this.responseText);
-
-        data.forEach((joke) => {
-            const li = document.createElement('li');
-            li.innerHTML = `<strong>${value}</strong>`;
-            document.querySelector('#results').appendChild(li);
-        });
-    }
-}
-
-xhr.send();
